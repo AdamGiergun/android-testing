8140ab5d133b3402ea81eb1d4f027583a59b2668
Adam Giergun <adam.giergun@gmail.com>
2022-01-15 19:27:49 +0100

    Migrate TasksActivity.kt to DataBinding

--- app/src/main/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksActivity.kt
+++ app/src/main/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksActivity.kt
@@ -18,6 +18,7 @@ package com.example.android.architecture.blueprints.todoapp.tasks
 import android.app.Activity
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
@@ -26,30 +27,31 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.android.architecture.blueprints.todoapp.R
-import com.google.android.material.navigation.NavigationView
+import com.example.android.architecture.blueprints.todoapp.databinding.TasksActBinding
 
 /**
  * Main activity for the todoapp. Holds the Navigation Host Fragment and the Drawer, Toolbar, etc.
  */
 class TasksActivity : AppCompatActivity() {
 
+    private lateinit var binding: TasksActBinding
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.tasks_act)
-        setupNavigationDrawer()
-        setSupportActionBar(findViewById(R.id.toolbar))
-
-        val navController: NavController = findNavController(R.id.nav_host_fragment)
-        appBarConfiguration =
-            AppBarConfiguration.Builder(R.id.tasks_fragment_dest, R.id.statistics_fragment_dest)
-                .setOpenableLayout(drawerLayout)
-                .build()
-        setupActionBarWithNavController(navController, appBarConfiguration)
-        findViewById<NavigationView>(R.id.nav_view)
-            .setupWithNavController(navController)
+        DataBindingUtil.setContentView<TasksActBinding>(this, R.layout.tasks_act).run {
+            binding = this
+            setupNavigationDrawer()
+            setSupportActionBar(toolbar)
+            val navController: NavController = this@TasksActivity.findNavController(R.id.nav_host_fragment)
+            appBarConfiguration =
+                AppBarConfiguration.Builder(R.id.tasks_fragment_dest, R.id.statistics_fragment_dest)
+                    .setOpenableLayout(drawerLayout)
+                    .build()
+            setupActionBarWithNavController(navController, appBarConfiguration)
+            navView.setupWithNavController(navController)
+        }
     }
 
     override fun onSupportNavigateUp(): Boolean {
@@ -58,8 +60,7 @@ class TasksActivity : AppCompatActivity() {
     }
 
     private fun setupNavigationDrawer() {
-        drawerLayout = (findViewById<DrawerLayout>(R.id.drawer_layout))
-            .apply {
+        drawerLayout = binding.drawerLayout.apply {
                 setStatusBarBackground(R.color.colorPrimaryDark)
             }
     }
