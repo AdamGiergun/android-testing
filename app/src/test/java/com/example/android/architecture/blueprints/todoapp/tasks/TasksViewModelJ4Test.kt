ecb74c53695524d8f8c8da4830f28ede10de68f5
Adam Giergun <adam.giergun@gmail.com>
2022-01-25 20:34:21 +0100

    Add LiveDataTestUtil.kt and modify TasksViewModelJ4Test.kt to use getOrAwaitValue from LiveDataTestUtil.kt

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelJ4Test.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelJ4Test.kt
@@ -1,14 +1,12 @@
 package com.example.android.architecture.blueprints.todoapp.tasks
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.lifecycle.Observer
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.example.android.architecture.blueprints.todoapp.Event
+import com.example.android.architecture.blueprints.todoapp.getOrAwaitValue
 import org.hamcrest.CoreMatchers.not
 import org.hamcrest.CoreMatchers.nullValue
 import org.hamcrest.MatcherAssert.assertThat
-import org.junit.Assert.*
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -24,19 +22,12 @@ class TasksViewModelJ4Test {
         // Given a fresh ViewModel instance
         val tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
 
-        val observer = Observer<Event<Unit>> {}
 
-        try {
-            tasksViewModel.newTaskEvent.observeForever(observer)
+        // When adding a new task
+        tasksViewModel.addNewTask()
 
-            // When adding a new task
-            tasksViewModel.addNewTask()
-
-            // Then the new task event is triggered
-            val value = tasksViewModel.newTaskEvent.value
-            assertThat(value?.getContentIfNotHandled(), (not(nullValue())))
-        } finally {
-            tasksViewModel.newTaskEvent.removeObserver(observer)
-        }
+        // Then the new task event is triggered
+        val value = tasksViewModel.newTaskEvent.getOrAwaitValue()
+        assertThat(value.getContentIfNotHandled(), not(nullValue()))
     }
 }
