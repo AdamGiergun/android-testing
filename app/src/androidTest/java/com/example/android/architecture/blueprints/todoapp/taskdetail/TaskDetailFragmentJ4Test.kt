f61528cb07495bec6cacbb0d5e3d028de5144d57
Adam Giergun <adasss@poczta.onet.pl>
2022-02-04 14:52:50 +0100

    Add completedTaskDetails_DisplayedInUi in TaskDetailFragmentJ4Test.kt

--- app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/taskdetail/TaskDetailFragmentJ4Test.kt
+++ app/src/androidTest/java/com/example/android/architecture/blueprints/todoapp/taskdetail/TaskDetailFragmentJ4Test.kt
@@ -44,22 +44,49 @@ class TaskDetailFragmentJ4Test {
     fun activeTaskDetails_DisplayedInUi() = runTest(UnconfinedTestDispatcher()) {
         // GIVEN - Add active (incomplete) task to the DB
         val activeTask = Task("Active task", "AndroidX Rocks", false)
-
         repository.saveTask(activeTask)
 
         // WHEN - Details fragment launched to display task
-        val bundle = TaskDetailFragmentArgs(activeTask.id).toBundle()
-        launchFragmentInContainer<TaskDetailFragment>(bundle, R.style.AppTheme)
+        launchDetailsFragment(activeTask.id)
 
         // THEN - Task details are displayed on the screen
         // make sure that the title/description are both shown and correct
+        assertThatTaskIsDisplayedCorrectly("Active task", "AndroidX Rocks", false)
+    }
+
+    @Test
+    fun completedTaskDetails_DisplayedInUi() = runTest(UnconfinedTestDispatcher()) {
+        // GIVEN - Add completed task to the DB
+        val completedTask = Task("Completed task", "AndroidX Rocks Even More", true)
+        repository.saveTask(completedTask)
+
+        // WHEN - Details fragment launched to display task
+        launchDetailsFragment(completedTask.id)
+
+        // THEN - Task details are displayed on the screen
+        // make sure that the title/description are both shown and correct
+        assertThatTaskIsDisplayedCorrectly("Completed task", "AndroidX Rocks Even More", true)
+    }
+
+    private fun launchDetailsFragment(taskId: String) {
+        val bundle = TaskDetailFragmentArgs(taskId).toBundle()
+        launchFragmentInContainer<TaskDetailFragment>(bundle, R.style.AppTheme)
+    }
+
+    private fun assertThatTaskIsDisplayedCorrectly(
+        taskTitle: String,
+        taskDescription: String,
+        taskIsCompleted: Boolean
+    ) {
         onView(withId(R.id.task_detail_title_text)).check(matches(isDisplayed()))
-        onView(withId(R.id.task_detail_title_text)).check(matches(withText("Active task")))
+        onView(withId(R.id.task_detail_title_text)).check(matches(withText(taskTitle)))
 
         onView(withId(R.id.task_detail_description_text)).check(matches(isDisplayed()))
-        onView(withId(R.id.task_detail_description_text)).check(matches(withText("AndroidX Rocks")))
+        onView(withId(R.id.task_detail_description_text)).check(matches(withText(taskDescription)))
 
         onView(withId(R.id.task_detail_complete_checkbox)).check(matches(isDisplayed()))
-        onView(withId(R.id.task_detail_complete_checkbox)).check(matches(not(isChecked())))
+        onView(withId(R.id.task_detail_complete_checkbox)).check(
+            matches(if (taskIsCompleted) isChecked() else not(isChecked()))
+        )
     }
 }
